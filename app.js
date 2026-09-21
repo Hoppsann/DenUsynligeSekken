7ba85066adefb64eb99fabef7ea67bd153fddde9
Andreas <andreas.lohne.24@gmail.com>
2026-09-21 10:57:11 +0200

    Added a working form and some styling

--- app.js
+++ app.js
@@ -5,9 +5,27 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"))
+
+
+
 app.get("/", (req, res) =>{
     res.render("index")
 })
+
+app.get("/sekken", (req, res) =>{
+    res.render("sekken")
+})
+
+app.post("/sekken", (req, res) =>{
+    const {sekk} = Number(req.body);
+
+
+
+    
+    res.redirect("/sekken")
+})
+
+
 
 app.listen(4000, () =>{
     console.log("http://localhost:4000")
