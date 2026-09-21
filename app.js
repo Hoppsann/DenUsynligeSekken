70ac081957bd0f9afe02e0b8196d78bd8871f237
Andreas <andreas.lohne.24@gmail.com>
2026-09-21 11:48:50 +0200

    added a number system for psycological health

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"))
 
-
+let total_psycological_weight = 0;
 
 app.get("/", (req, res) =>{
     res.render("index")
@@ -17,9 +17,10 @@ app.get("/sekken", (req, res) =>{
 })
 
 app.post("/sekken", (req, res) =>{
-    const {sekk} = Number(req.body);
-
-
+    const {sekk} = req.body;
+    const weight_amount = Number(sekk)
+    console.log(weight_amount)
+    
 
     
     res.redirect("/sekken")
